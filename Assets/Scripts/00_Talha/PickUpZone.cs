4b273a9fe54f0b08fef5751460035f52b0c615ba
MTA25 <128969466+MTA25@users.noreply.github.com>
2023-09-08 12:40:05 -0700

    Bla Bla

--- Assets/Scripts/00_Talha/PickUpZone.cs
+++ Assets/Scripts/00_Talha/PickUpZone.cs
@@ -8,59 +8,57 @@ public class PickUpZone : MonoBehaviour
 {
     [SerializeField] private ItemsDataContainer m_SpawnedItemsData;
     private ItemsDataContainer m_PlayerItemsDataContainer = new();
-    private CubePickup m_Playerdata = new();
-    private Vector3 SpawnPosition = new();
+    private PlayerDataContainer m_Playerdata = new();
+    private Vector3 SpawnPosition;
     private Coroutine _coroutine;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // Get Player Container
             m_PlayerItemsDataContainer = other.GetComponent<ItemsDataContainer>();
-            m_Playerdata = other.GetComponent<CubePickup>();
             
-            if (m_PlayerItemsDataContainer.Items.Count == 0)
-            {
-                SpawnPosition = new Vector3(0,0,0);
-            }
-            else
-            {
-                SpawnPosition = m_PlayerItemsDataContainer.Items[m_PlayerItemsDataContainer.Items.Count - 1].transform.position + new Vector3(0, 0.5f, 0);
-            }
+            m_Playerdata = other.GetComponent<PlayerDataContainer>();
             
-
             _coroutine = StartCoroutine(Delay());
         }
     }
 
     IEnumerator Delay()
     {
-        if (m_SpawnedItemsData.Items.Count < 0)
+        if (m_SpawnedItemsData.Items.Count <= 0)
         {
             if (_coroutine != null)
             {
                 StopCoroutine(_coroutine);
             }
         }
-
-
-        if (m_SpawnedItemsData != null)
-        {
-            m_SpawnedItemsData.Items[m_SpawnedItemsData.Items.Count - 1].transform.SetParent(m_Playerdata.playerHand);
-
-            var position = SpawnPosition;
-            m_SpawnedItemsData.Items[^1].transform.DOMove(position, 0.5f);
-
-            //m_SpawnedItemsData.Items[m_SpawnedItemsData.Items.Count - 1].transform.position = SpawnPosition.position;
-
-            position += new Vector3(0, 0.5f, 0);
-            SpawnPosition = position;
-
-            m_PlayerItemsDataContainer.Items.Add(m_SpawnedItemsData.Items[m_SpawnedItemsData.Items.Count - 1]);
-            m_SpawnedItemsData.Items.Remove(m_SpawnedItemsData.Items[m_SpawnedItemsData.Items.Count - 1]);
-        }
-
-        yield return new WaitForSeconds(0.5f);
         
+        if (m_PlayerItemsDataContainer.Items.Count == 0)
+        {
+            SpawnPosition = Vector3.zero;
+        }
+        else
+        {
+            SpawnPosition = m_PlayerItemsDataContainer.Items[m_PlayerItemsDataContainer.Items.Count - 1].transform.position + new Vector3(0, 0.5f, 0);
+        }
+        
+
+        int index = m_SpawnedItemsData.Items.Count - 1;
+
+        m_SpawnedItemsData.Items[index].transform.SetParent(m_Playerdata.playerHand);
+        
+        m_PlayerItemsDataContainer.Items.Add(m_SpawnedItemsData.Items[index]);
+        
+        m_SpawnedItemsData.Items.Remove(m_SpawnedItemsData.Items[index]);
+
+        //m_SpawnedItemsData.Items[index].transform.DOMove(SpawnPosition, 0.5f);
+
+        m_PlayerItemsDataContainer.Items[m_PlayerItemsDataContainer.Items.Count - 1].transform.position = Vector3.zero;
+        m_PlayerItemsDataContainer.Items[m_PlayerItemsDataContainer.Items.Count - 1].transform.localPosition = SpawnPosition;
+        
+        yield return new WaitForSeconds(0.5f);
+
         _coroutine = StartCoroutine(Delay());
     }
 
